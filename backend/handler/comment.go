10dfdf4f254424608772ae738b7dc68b2a93c6e3
Jinvic <77521861+Jinvic@users.noreply.github.com>
2025-01-25 08:39:10 +0000

    fix: 随机生成用户名写入cookie

--- backend/handler/comment.go
+++ backend/handler/comment.go
@@ -4,6 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/kingwrcy/moments/db"
 	"github.com/kingwrcy/moments/vo"
@@ -11,11 +17,6 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/samber/do/v2"
 	"gorm.io/gorm"
-	"io"
-	"net/http"
-	"net/url"
-	"strconv"
-	"time"
 )
 
 type CommentHandler struct {
@@ -149,7 +150,42 @@ func (c CommentHandler) AddComment(ctx echo.Context) error {
 	}
 
 	if comment.Username == "" {
-		comment.Username = fmt.Sprintf("匿名用户_%s", uuid.New().String()[:4])
+		// 尝试从 Cookie 中获取用户名
+		cookie, err := ctx.Cookie("anonymous_username")
+		var username string
+
+		if err != nil || cookie.Value == "" {
+			// 如果 Cookie 不存在，生成一个新的随机用户名
+			username = fmt.Sprintf("匿名用户_%s", uuid.New().String()[:4])
+			// 对用户名进行 URL 编码
+			encodedUsername := url.QueryEscape(username)
+			// 设置 Cookie，有效期 7 天
+			ctx.SetCookie(&http.Cookie{
+				Name:    "anonymous_username",
+				Value:   encodedUsername,
+				Path:    "/",
+				Expires: time.Now().Add(7 * 24 * time.Hour),
+			})
+		} else {
+			// 如果 Cookie 存在，使用之前的用户名
+			decodedUsername, err := url.QueryUnescape(cookie.Value)
+			if err != nil {
+				// 生成一个新的随机用户名
+				username = fmt.Sprintf("匿名用户_%s", uuid.New().String()[:4])
+				// 对用户名进行 URL 编码
+				encodedUsername := url.QueryEscape(username)
+				// 设置 Cookie，有效期 7 天
+				ctx.SetCookie(&http.Cookie{
+					Name:    "anonymous_username",
+					Value:   encodedUsername,
+					Path:    "/",
+					Expires: time.Now().Add(7 * 24 * time.Hour),
+				})
+			} else {
+				username = decodedUsername
+			}
+		}
+		comment.Username = username
 	}
 
 	comment.Content = req.Content
