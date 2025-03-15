a7dcec551288f9bd463c07f7238f033926bbcb41
xuewenG <gexuewen@outlook.com>
2025-03-16 00:05:59 +0800

    feat: 优化夜间样式和图片样式

--- front/components/Header.vue
+++ front/components/Header.vue
@@ -1,39 +1,80 @@
 <template>
-  <div class="header relative mb-14" v-if="$route.path!=='/new' && $route.path.indexOf('/edit/') < 0">
-    <div v-if="$route.path!=='/' && $route.path.indexOf('/memo/') < 0" :class="{ 'bg-[#4c4c4c]/80 z-10': y > 100 }" class="flex fixed justify-between items-center p-4 w-full md:w-[567px] text-white top-0">
+  <div
+    v-if="$route.path !== '/new' && $route.path.indexOf('/edit/') < 0"
+    class="header relative mb-14"
+  >
+    <div
+      v-if="$route.path !== '/' && $route.path.indexOf('/memo/') < 0"
+      :class="{ 'bg-[#4c4c4c]/80 z-10': y > 100 }"
+      class="flex fixed justify-between items-center p-4 w-full md:w-[567px] text-white top-0"
+    >
       <NuxtLink class="flex items-center" title="返回主页">
-        <UIcon @click="navigateTo('/')" name="i-carbon-chevron-left" class="w-5 h-5 cursor-pointer mr-4"/>
-        <span v-if="$route.path==='/user/calendar'">日历检索</span>
-        <span v-else-if="$route.path==='/sys/settings'">系统设置</span>
-        <span v-else-if="$route.path==='/user/settings'">用户中心</span>
+        <UIcon
+          @click="navigateTo('/')"
+          name="i-carbon-chevron-left"
+          class="w-5 h-5 cursor-pointer mr-4"
+        />
+        <span v-if="$route.path === '/user/calendar'">日历检索</span>
+        <span v-else-if="$route.path === '/sys/settings'">系统设置</span>
+        <span v-else-if="$route.path === '/user/settings'">用户中心</span>
         <span v-else-if="$route.path.indexOf('/tags/') >= 0">话题专栏</span>
         <span v-else>
-          <span v-if="!global.userinfo.token && $route.path==='/user/login'">登录</span>
-          <span v-else-if="!global.userinfo.token && $route.path==='/user/reg'">注册</span>
+          <span v-if="!global.userinfo.token && $route.path === '/user/login'">
+            登录
+          </span>
+          <span
+            v-else-if="!global.userinfo.token && $route.path === '/user/reg'"
+          >
+            注册
+          </span>
           <span v-else>{{ props.user.nickname }} 的空间</span>
         </span>
       </NuxtLink>
-      <NuxtLink @click="logout" v-if="$route.path === '/user/settings' && global.userinfo.token" title="登出">
-        <UIcon name="i-carbon-logout" class="w-5 h-5 cursor-pointer"/>
+      <NuxtLink
+        v-if="$route.path === '/user/settings' && global.userinfo.token"
+        title="登出"
+        @click="logout"
+      >
+        <UIcon name="i-carbon-logout" class="w-5 h-5 cursor-pointer" />
       </NuxtLink>
     </div>
 
     <div
-        class="dark:bg-neutral-800 hidden sm:flex sm:absolute sm:-right-10 sm:rounded sm:p-2 sm:flex-col sm:w-fit justify-end shadow w-full flex-row top-0  p-1 flex  gap-2 bg-white ">
-      <svg v-if="mode==='light'" @click="toggleMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-           viewBox="0 0 24 24" fill="none"
-           stroke="#FDE047"
-           stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-           class="lucide lucide-moon-star-icon cursor-pointer">
+      class="dark:bg-neutral-800 hidden sm:flex sm:absolute sm:-right-10 sm:rounded sm:p-2 sm:flex-col sm:w-fit justify-end shadow w-full flex-row top-0 p-1 flex gap-2 bg-white"
+    >
+      <svg
+        v-if="mode.value === 'light'"
+        class="lucide lucide-moon-star-icon cursor-pointer"
+        @click="toggleMode"
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#FDE047"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
         <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9"></path>
         <path d="M20 3v4"></path>
         <path d="M22 5h-4"></path>
       </svg>
 
-      <svg v-else @click="toggleMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-           fill="none"
-           stroke="#FDE047" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-           class="lucide lucide-sun-icon cursor-pointer">
+      <svg
+        v-else
+        class="lucide lucide-sun-icon cursor-pointer"
+        @click="toggleMode"
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#FDE047"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
         <circle cx="12" cy="12" r="4"></circle>
         <path d="M12 2v2"></path>
         <path d="M12 20v2"></path>
@@ -45,62 +86,95 @@
         <path d="m19.07 4.93-1.41 1.41"></path>
       </svg>
 
+      <NuxtLink v-if="global.userinfo.token" to="/new" title="发表">
+        <UIcon
+          name="i-carbon-camera"
+          class="text-[#9fc84a] w-5 h-5 cursor-pointer"
+        />
+      </NuxtLink>
+      <NuxtLink
+        v-if="$route.path !== '/user/calendar' && global.userinfo.token"
+        to="/user/calendar"
+        title="日历检索"
+      >
+        <UIcon
+          name="i-jam-search-folder"
+          class="text-[#9fc84a] w-5 h-5 cursor-pointer"
+        />
+      </NuxtLink>
 
-      <NuxtLink to="/new" v-if="global.userinfo.token " title="发表">
-        <UIcon name="i-carbon-camera" class="text-[#9fc84a] w-5 h-5 cursor-pointer"/>
+      <NuxtLink
+        v-if="$route.path !== '/sys/settings' && global.userinfo.id === 1"
+        to="/sys/settings"
+        title="系统设置"
+      >
+        <UIcon
+          name="i-carbon-settings"
+          class="text-[#9fc84a] w-5 h-5 cursor-pointer"
+        />
       </NuxtLink>
-      <NuxtLink to="/user/calendar" v-if="$route.path !== '/user/calendar' && global.userinfo.token" title="日历检索">
-        <UIcon name="i-jam-search-folder" class="text-[#9fc84a] w-5 h-5 cursor-pointer"/>
+      <NuxtLink
+        v-if="$route.path !== '/user/settings' && global.userinfo.token"
+        to="/user/settings"
+        title="用户中心"
+      >
+        <UIcon
+          name="i-carbon-user-avatar"
+          class="text-[#9fc84a] w-5 h-5 cursor-pointer"
+        />
       </NuxtLink>
-
-      <NuxtLink to="/sys/settings" v-if="$route.path !== '/sys/settings' && global.userinfo.id === 1" title="系统设置">
-        <UIcon name="i-carbon-settings" class="text-[#9fc84a] w-5 h-5 cursor-pointer"/>
-      </NuxtLink>
-      <NuxtLink to="/user/settings" v-if="$route.path !== '/user/settings' && global.userinfo.token" title="用户中心">
-        <UIcon name="i-carbon-user-avatar" class="text-[#9fc84a] w-5 h-5 cursor-pointer"/>
-      </NuxtLink>
-      <NuxtLink to="/user/login" v-if="!global.userinfo.token" title="登录">
-        <UIcon name="i-carbon-login" class="text-[#9fc84a] w-5 h-5 cursor-pointer"/>
+      <NuxtLink v-if="!global.userinfo.token" to="/user/login" title="登录">
+        <UIcon
+          name="i-carbon-login"
+          class="text-[#9fc84a] w-5 h-5 cursor-pointer"
+        />
       </NuxtLink>
     </div>
 
-    <img class="header-img w-full" :src="props.user.coverUrl"
-         alt="">
+    <img class="header-img w-full" :src="props.user.coverUrl" alt="" />
     <div class="absolute right-2 bottom-[-40px]">
       <div class="userinfo flex flex-col">
         <div class="flex flex-row items-center gap-4 justify-end">
-          <div class="username text-lg font-bold text-white">{{ props.user.nickname }}</div>
-          <img :src="props.user.avatarUrl"
-               class="avatar w-[70px] h-[70px] rounded-xl"></div>
-        <div class="slogon text-gray truncate w-full text-end text-xs mt-2">{{ props.user.slogan }}</div>
+          <div class="username text-lg font-bold text-white">
+            {{ props.user.nickname }}
+          </div>
+          <img
+            :src="props.user.avatarUrl"
+            class="avatar w-[70px] h-[70px] rounded-xl"
+          />
+        </div>
+        <div class="slogon text-gray truncate w-full text-end text-xs mt-2">
+          {{ props.user.slogan }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import type {UserVO} from "~/types";
-import {useGlobalState} from "~/store";
-import {useColorMode} from '@vueuse/core'
+import { toast } from "vue-sonner";
+import type { UserVO } from "~/types";
+import { useGlobalState } from "~/store";
 
-const global = useGlobalState()
-const props = defineProps<{ user: UserVO }>()
-const mode = useColorMode()
-const {y} = useWindowScroll()
+const global = useGlobalState();
+const props = defineProps<{ user: UserVO }>();
+const mode = useColorMode();
+const { y } = useWindowScroll();
 
 const logout = async () => {
-  global.value.userinfo = {}
-  await navigateTo('/')
-}
+  global.value.userinfo = {};
+  await navigateTo("/");
+};
 
 const toggleMode = () => {
-  if (mode.value === 'dark') {
-    mode.value = 'light'
+  if (mode.preference === "system") {
+    mode.preference = "dark";
+  } else if (mode.preference === "dark") {
+    mode.preference = "light";
   } else {
-    mode.value = 'dark'
+    mode.preference = "system";
+    toast.success("显示模式将跟随系统设置");
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- front/components/UploadImagePreview.vue
+++ front/components/UploadImagePreview.vue
@@ -1,12 +1,29 @@
 <template>
-  <div ref="el" v-if="($route.path.startsWith('/new') || $route.path.startsWith('/edit')) && images.length>0"
-       :style="gridStyle" class="grid gap-2">
-    <div :key="img" v-for="img in images" class="relative">
-      <img :src="img" alt="" class="cursor-move rounded relative"
-           :class="images.length === 1 ? 'full-cover-image-single' : 'full-cover-image-mult'">
-      <div class="absolute right-6 top-0 px-1 bg-white m-2 rounded hover:text-red-500 cursor-pointer"
-           @click="removeImage(img)">
-        <UIcon name="i-carbon-trash-can" class=""/>
+  <div
+    v-if="
+      images.length > 0 &&
+      ($route.path.startsWith('/new') || $route.path.startsWith('/edit'))
+    "
+    ref="el"
+    class="grid gap-2"
+    :style="gridStyle"
+  >
+    <div
+      v-for="img in images"
+      :key="img"
+      class="relative"
+      :class="
+        images.length === 1
+          ? 'full-cover-image-single'
+          : 'full-cover-image-mult'
+      "
+    >
+      <img :src="img" class="cursor-move rounded" />
+      <div
+        class="absolute top-0 right-0 px-1 bg-white dark:bg-gray-900 m-2 rounded hover:text-red-500 cursor-pointer"
+        @click="removeImage(img)"
+      >
+        <UIcon name="i-carbon-trash-can" />
       </div>
     </div>
   </div>
@@ -17,12 +34,15 @@
         v-for="(imgConfig, z) in props.imgConfigs"
         :key="z"
         :href="imgConfig.url"
-        :class="images.length === 1 ? 'full-cover-image-single' : 'full-cover-image-mult'"
+        :class="
+          images.length === 1
+            ? 'full-cover-image-single'
+            : 'full-cover-image-mult'
+        "
       >
         <img
           class="cursor-zoom-in rounded"
           :src="imgConfig.thumbUrl"
-          alt=""
           :onerror="`javascript:this.src='${imgConfig.url}';this.onerror=null`"
         />
       </div>
@@ -31,96 +51,87 @@
 </template>
 
 <script setup lang="ts">
-import {useSortable} from '@vueuse/integrations/useSortable'
-import type {SysConfigVO} from "~/types";
+import { useSortable } from "@vueuse/integrations/useSortable";
+import type { SysConfigVO } from "~/types";
 
 interface ImgConfig {
-  url: string
-  thumbUrl: string
+  url: string;
+  thumbUrl: string;
 }
 
-const sysConfig = useState<SysConfigVO>('sysConfig')
-const route = useRoute()
-const el = ref(null)
-const props = defineProps<{ imgs: string, imgConfigs?: ImgConfig[] }>()
-const emit = defineEmits(['removeImage', 'dragImage'])
-const images = ref<string[]>(
-  (props.imgs || '').split(",").filter(Boolean)
-)
+const sysConfig = useState<SysConfigVO>("sysConfig");
+const route = useRoute();
+const el = ref(null);
+const props = defineProps<{ imgs: string; imgConfigs?: ImgConfig[] }>();
+const emit = defineEmits(["removeImage", "dragImage"]);
+const images = ref<string[]>((props.imgs || "").split(",").filter(Boolean));
 
 watch(props, () => {
-  images.value = (props.imgs || '').split(",").filter(Boolean)
-})
+  images.value = (props.imgs || "").split(",").filter(Boolean);
+});
 
 watch(images, () => {
-  emit('dragImage', images.value)
-})
+  emit("dragImage", images.value);
+});
 
 const removeImage = async (img: string) => {
-  await useMyFetch('/memo/removeImage', {
-    img
-  })
-  emit('removeImage', img)
-}
+  await useMyFetch("/memo/removeImage", {
+    img,
+  });
+  emit("removeImage", img);
+};
 
 onMounted(() => {
-  if (route.path.startsWith('/new') || route.path.startsWith('/edit')) {
+  if (route.path.startsWith("/new") || route.path.startsWith("/edit")) {
     setTimeout(() => {
-      useSortable(el, images)
-    }, 500)
+      useSortable(el, images);
+    }, 500);
   }
-})
+});
 
 const gridStyle = computed(() => {
-  let style = 'max-width:100%;display:grid;gap: 0.5rem;align-items: start;'; // 确保内容顶部对齐
+  let style = "max-width:100%; display:grid; gap: 0.5rem; align-items: start;"; // 确保内容顶部对齐
   switch (images.value.length) {
     case 1:
-      style += 'grid-template-columns: 1fr;max-width:60%;';
+      style += "grid-template-columns: 1fr; max-width:60%;";
       break;
     case 2:
-      style += 'grid-template-columns: 1fr 1fr; aspect-ratio: 2 / 1;';
+      style += "grid-template-columns: 1fr 1fr; aspect-ratio: 2 / 1;";
       break;
     case 3:
-      style += 'grid-template-columns: 1fr 1fr 1fr; aspect-ratio: 3 / 1;';
+      style += "grid-template-columns: 1fr 1fr 1fr; aspect-ratio: 3 / 1;";
       break;
     case 4:
-      style += 'grid-template-columns: 1fr 1fr; aspect-ratio: 1;';
+      style += "grid-template-columns: 1fr 1fr; aspect-ratio: 1;";
       break;
     default:
-      style += 'grid-template-columns: 1fr 1fr 1fr;';
+      style += "grid-template-columns: 1fr 1fr 1fr;";
   }
   return style;
 });
 </script>
 
-
 <style scoped>
 .full-cover-image-mult {
-  max-height: 300px;
   width: 100%;
+  max-height: 300px;
   aspect-ratio: 1 / 1;
 
-  >img {
+  > img {
     width: 100%;
     height: 100%;
     object-fit: cover;
     object-position: center;
-    border: 1px solid transparent;
   }
 }
 
 .full-cover-image-single {
-  max-height: 300px;
-  height: auto;
-  width: auto;
+  width: fit-content;
 
-  >img {
-    width: auto;
-    height: auto;
+  > img {
     max-height: 300px;
     object-fit: cover;
     object-position: center;
-    border: 1px solid transparent;
   }
 }
 </style>
