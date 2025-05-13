77cf17c308053a04657622666b1668f0a010fd2f
dinphy <dinphy1205@126.com>
2025-05-13 13:29:10 +0800

    feat(ref): 优化拟态窗口

--- front/components/Memo.vue
+++ front/components/Memo.vue
@@ -186,7 +186,7 @@
               v-model="moreToolbar"
               :ui="{
                 container:
-                  'fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center',
+                  'fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center backdrop-blur',
               }"
             >
               <div
--- front/components/MobileNav.vue
+++ front/components/MobileNav.vue
@@ -3,7 +3,7 @@
     v-model="open"
     :ui="{
       container:
-        'fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center',
+        'fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center backdrop-blur',
     }"
   >
     <div
