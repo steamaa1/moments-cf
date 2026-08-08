b16f9baa42bbcdfd14081541dafbc19b6abe4016
steamaa1 <170596778+steamaa1@users.noreply.github.com>
2026-08-08 04:24:58 +0000

    fix: 评论身份输入改为一行三个短框（grid 三列，不换行不挤压）

--- front/components/CommentBox.vue
+++ front/components/CommentBox.vue
@@ -9,11 +9,11 @@
       </div>
     </div>
     <Emoji v-if="emojiShow" @selected="emojiSelected"/>
-    <div v-if="userShow" class="flex flex-wrap gap-1">
+    <div v-if="userShow" class="grid grid-cols-3 gap-1">
       <template v-if="!global.userinfo.token">
-        <UInput placeholder="姓名" v-model="state.username"/>
-        <UInput placeholder="网站" v-model="state.website"/>
-        <UInput placeholder="邮箱" v-model="state.email"/>
+        <UInput class="min-w-0" placeholder="姓名" v-model="state.username"/>
+        <UInput class="min-w-0" placeholder="网站" v-model="state.website"/>
+        <UInput class="min-w-0" placeholder="邮箱" v-model="state.email"/>
       </template>
     </div>
   </div>
